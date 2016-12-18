6416f5c7ff561375bf9240652c7a152a22925ea4
milovanderlinden <milo@dogodigi.net>
2016-12-18 18:12:17 +0100

    Fixing minor configuration errors

--- cartodb-sql-api/run.sh
+++ cartodb-sql-api/run.sh
@@ -3,14 +3,12 @@ cd /windshaft-cartodbdocker
 
 mv config/_config config/environments/$CARTO_ENV.js.example
 
-sed -i "s/__CARTO_ENV__/$CARTO_ENV/g" config/environments/$CARTO_ENV.js.example
 sed -i "s/__DB_PORT__/$DB_PORT/g" config/environments/$CARTO_ENV.js.example
 sed -i "s/__DB_HOST__/$DB_HOST/g" config/environments/$CARTO_ENV.js.example
 sed -i "s/__DB_USER__/$DB_USER/g" config/environments/$CARTO_ENV.js.example
 sed -i "s/__REDIS_HOST__/$REDIS_HOST/g" config/environments/$CARTO_ENV.js.example
 sed -i "s/__REDIS_PORT__/$REDIS_PORT/g" config/environments/$CARTO_ENV.js.example
-sed -i "s/__WINDSHAFT_PORT__/$WINDSHAFT_PORT/g" config/environments/$CARTO_ENV.js.example
-sed -i "s/__CORS_ENABLED__/$CORS_ENABLED/g" config/environments/$CARTO_ENV.js.example
+sed -i "s/__CARTO_ENV__/$CARTO_ENV/g" config/environments/$CARTO_ENV.js.example
 
 ./configure --environment=$CARTO_ENV --with-pgport=$DB_PORT
 #env
