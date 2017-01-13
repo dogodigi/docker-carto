38bf29054995b3bf6b12b404ef8fa53867a7dab5
milovanderlinden <milo@dogodigi.net>
2017-01-13 01:14:20 +0100

    Template was not set because of wrong user

--- cartodb-postgis/run.sh
+++ cartodb-postgis/run.sh
@@ -7,7 +7,7 @@ export POSTGRES_DB=template_postgis
 # Add carto functions to the database template
 createdb -T template0 -O $PGUSER -E UTF8 $POSTGRES_DB
 createlang plpgsql -d $POSTGRES_DB
-psql --dbname="$POSTGRES_DB" <<EOSQL
+psql -U postgres -d $POSTGRES_DB <<EOSQL
 CREATE EXTENSION postgis;
 CREATE EXTENSION pgrouting;
 CREATE EXTENSION cartodb CASCADE;
