5480ae2e1095a2eae75e116ccbbc892fb81b1b36
milovanderlinden <milo@dogodigi.net>
2017-01-13 01:18:25 +0100

    Minor fixes

--- cartodb-postgis/run.sh
+++ cartodb-postgis/run.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
-set -e
 
-# Perform all actions as user 'postgres'
-export PGUSER=postgres
-export POSTGRES_DB=template_postgis
-# Add carto functions to the database template
-createdb -T template0 -O $PGUSER -E UTF8 $POSTGRES_DB
-createlang plpgsql -d $POSTGRES_DB
-psql -U postgres -d $POSTGRES_DB <<EOSQL
+# Add required extension to the database template
+createdb -T template0 -O $PGUSER -E UTF8 $DB_TEMPLATE
+createlang plpgsql -d $DB_TEMPLATE
+psql -U postgres -d $DB_TEMPLATE <<EOSQL
 CREATE EXTENSION postgis;
 CREATE EXTENSION pgrouting;
 CREATE EXTENSION cartodb CASCADE;
 EOSQL
+echo
+echo 'Carto database complete; ready for start up.'
+echo
